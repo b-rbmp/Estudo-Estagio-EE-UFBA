684d5a83797b328d3704112d45db43884401acd7
b-rbmp <4665417+b-rbmp@users.noreply.github.com>
2021-06-03 17:53:44 -0300

    Cache + Updated Data + Finished App + TOC
    Loading using cache instead of reloading, updated data and finished all sections of the app, including adding a Table of Contents on the sidebar

--- app.py
+++ app.py
@@ -1,34 +1,116 @@
 # -*- coding: utf-8 -*-
 
-# APENAS ESTÁGIOS NA REGIÃO PROXIMA DE SALVADOR! E TIRANDO MARKETING/COMERCIAL
-# Periodo: Data limite para enviar curriculo.
-# Se indisponível, data que a vaga foi postada pela primeira vez
-# Para requisito e maximo semestre, as vezes é utilizado o periodo pedido em relação a
-# previsão de formatura. Ex: Disponibilidade para estagiar pelo menos 1 ano: Maximo_semestre = 10 - 2 = 8
-# Informações conforme informado na divulgação das empresas pelos
-# Centro de Atendimento à Graduação - CEAG Politecnica e Colegiado de Engenharia Elétrica
-# Vagas em desenvolvimento de Software são ignoradas (consideradas como Eng Computação/Ciencia
-# da Computação) a menos que vinculo claro com alguma área de Eng Elétrica/Eletrônica/Hardware
-# Para grandes empresas, foi feito uma pesquisa no linkedin OU google para observar se as empresas
-# ofereciam vagas de estágio em áreas relacionadas a engenharia elétrica. (quando não informavam nos sites
-# de inscrição ou nos flyers)
+""" Estudo Estágio Engenharia Elétrica - UFBA
+
+Um estudo realizado para analisar os anuncios de estágio recebidos entre os semestres 2016.1 e 2021.1 
+através do Centro de Atendimento à Graduação - CEAG Politecnica e Colegiado de Engenharia Elétrica com 
+vagas para Engenharia Elétrica/Eletrônica
+Foram considerados apenas os estágios na região próxima de Salvador e desconsiderando vagas para Marketing/Comercial
+Periodo Considerado: Data limite para enviar curriculo. Se indisponível, data que a vaga foi postada pela primeira vez
+Para requisito e maximo semestre, as vezes é utilizado o periodo pedido em relação a
+previsão de formatura. Ex: Disponibilidade para estagiar pelo menos 1 ano: Maximo_semestre = 10 - 2 = 8
+Informações conforme informado na divulgação das empresas pelos
+Centro de Atendimento à Graduação - CEAG Politecnica e Colegiado de Engenharia Elétrica
+Vagas em desenvolvimento de Software são ignoradas (consideradas como Eng Computação/Ciencia
+da Computação) a menos que vinculo claro com alguma área de Eng Elétrica/Eletrônica/Hardware
+Para grandes empresas, foi feito uma pesquisa no linkedin OU google para observar se as empresas
+ofereciam vagas de estágio em áreas relacionadas a engenharia elétrica. (quando não informavam nos sites
+de inscrição ou nos flyers) """
+
 from typing import Dict
 import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
-import matplotlib.pyplot as plt
 from wordcloud import WordCloud, STOPWORDS
 
-dados_df = pd.read_json('estagio_data.json', encoding='UTF-8')
+# Define a configuração do streamlit
+st.set_page_config(page_title="Estudo Estágio",
+                   page_icon=":clipboard:",
+                   layout="centered",
+                   initial_sidebar_state="auto")
 
+
+# Função que adiciona um arquivo .css
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+local_css("style.css")
+
+# Esconde menu e footer
+hide_menu_footer = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_menu_footer, unsafe_allow_html=True) 
+
+# Função cacheada que carrega os dados
+@st.cache(persist=True)
+def load_data():
+    data = pd.read_json('estagio_data.json', encoding='UTF-8', dtype='object')
+    return data
+
+dados_df = load_data()
+
+# Desativa o modebar
 config_plotly = {'displayModeBar': False}
 
-# APP
+# SIDEBAR
+st.sidebar.title('Índice do Estudo:')
+st.sidebar.write('')
+st.sidebar.markdown(
+    '<a class="toc-header" href="#1-per-odo-vs-quantidade-de-anuncios">1. Período vs Quantidade de anuncios</a>',
+    unsafe_allow_html=True)
+st.sidebar.markdown(
+    '<a class="toc-header" href="#2-ofertas-de-est-gio-por-rea-sub-rea">2. Ofertas de estágio por Área/Subárea</a>',
+    unsafe_allow_html=True)
+st.sidebar.markdown(
+    '<span>- </span><a class="toc-sub-header" href="#2-1-divis-o-de-ofertas-por-rea">2.1. Divisão de ofertas por área</a>',
+    unsafe_allow_html=True)
+st.sidebar.markdown(
+    '<span>- </span><a class="toc-sub-header" href="#2-2-divis-o-por-sub-rea">2.2. Divisão por Subárea</a>',
+    unsafe_allow_html=True)
+st.sidebar.markdown(
+    '<a class="toc-header" href="#3-requisitos-de-semestraliza-o">3. Requisitos de Semestralização</a>',
+    unsafe_allow_html=True)
+st.sidebar.markdown(
+    '<span>- </span><a class="toc-sub-header" href="#3-1-semestraliza-o-m-nima">3.1. Semestralização Mínima</a>',
+    unsafe_allow_html=True)
+st.sidebar.markdown(
+    '<span>- </span><a class="toc-sub-header" href="#3-2-semestraliza-o-m-xima">3.2. Semestralização Máxima</a>',
+    unsafe_allow_html=True)
+st.sidebar.markdown(
+    '<a class="toc-header" href="#4-pr-requisitos-e-diferenciais">4. Pré-requisitos e diferenciais</a>',
+    unsafe_allow_html=True)
+st.sidebar.markdown(
+    '<span>- </span><a class="toc-sub-header" href="#4-1-pr-requisitos-mais-comuns">4.1. Pré-requisitos mais comuns</a>',
+    unsafe_allow_html=True)
+st.sidebar.markdown(
+    '<span>- </span><a class="toc-sub-header" href="#4-2-exist-ncia-de-pr-requisitos">4.2. Existência de Pré-requisitos</a>',
+    unsafe_allow_html=True)
+st.sidebar.markdown(
+    '<span>- </span><a class="toc-sub-header" href="#4-3-pr-requisitos-filtrados-por-rea-de-atua-o">4.3. Pré-requisitos filtrados por área de atuação</a>',
+    unsafe_allow_html=True)
+st.sidebar.markdown(
+    '<span>- </span><a class="toc-sub-header" href="#4-4-diferenciais-mais-comuns">4.4. Diferenciais mais comuns</a>',
+    unsafe_allow_html=True)
+st.sidebar.markdown(
+    '<a class="toc-header" href="#5-carga-hor-ria-semanal">5. Carga Horária Semanal</a>',
+    unsafe_allow_html=True)
+st.sidebar.markdown(
+    '<a class="toc-header" href="#6-ofertas-por-empresa">6. Ofertas por Empresa</a>',
+    unsafe_allow_html=True)
+st.sidebar.markdown(
+    '<a class="toc-header" href="#7-navega-o-pelos-dados">7. Navegação pelos dados</a>',
+    unsafe_allow_html=True)
 
-st.title('Estudo Estágio Engenharia Elétrica UFBA')
-st.write('')
+# Conteúdo Principal
+st.title('Estudo Anuncios de Estágio EE - UFBA')
+st.markdown("""---""")
 st.write(
     'Um estudo realizado para analisar os anuncios de estágio recebidos entre os semestres 2016.1 e 2021.1 através do Centro de Atendimento à Graduação - CEAG Politecnica e Colegiado de Engenharia Elétrica com vagas para Engenharia Elétrica/Eletrônica'
 )
@@ -41,14 +123,17 @@ st.write(
 st.write(
     'Considerou-se também o período (semestre) com a data limite para envio do currículo/inscrição no Processo Seletivo, e para os requisito ou máximo semestre matriculado, as vezes é utilizado o periodo pedido em relação a previsão de formatura.'
 )
+st.markdown('Link para o repositório GitHub: [Clique Aqui](https://github.com/b-rbmp/Estudo-Estagio-EE-UFBA "Repositório GitHub")')
 
 st.header('1. Período vs Quantidade de anuncios')
 st.write(
     'Observando a quantidade de anuncios de estágio em cada semestre, verificou-se um aumento progressivo com os anos, interrompido durante o período de adaptação para o COVID-19 (2020.1 e 2020.2)'
 )
+
 # Conta quantos anuncios por semestre
 ofertas_por_semestre = dados_df['periodo'].value_counts()
-# Configura o plot pelo plotly
+
+# Configura o plot pelo plotly e o carrega usando a função correspondente do streamlit
 fig_ofertas_semestre = go.Figure(
     data=[go.Bar(x=ofertas_por_semestre.index, y=ofertas_por_semestre)])
 fig_ofertas_semestre.update_traces(text=ofertas_por_semestre,
@@ -74,8 +159,11 @@ st.write(
 st.write(
     'Por outro lado, das empresas que selecionam estagiários para vagas específicas, a área de potência é a mais requisitada, com mais de 1/4 dos anuncios para esta área, seguido de controle, eletrônica e computação.'
 )
+
+# Conta o número de ofertas por área
 ofertas_por_area = dados_df['area'].value_counts()
-# Configura o plot pelo plotly
+
+# Configura o plot pelo plotly e o carrega usando a função correspondente do streamlit
 fig_ofertas_por_area = go.Figure(data=[
     go.Pie(labels=ofertas_por_area.index,
            values=ofertas_por_area,
@@ -101,6 +189,8 @@ st.subheader('2.2. Divisão por Subárea')
 st.write(
     'Observa-se também a divisão dos anuncios de estágio por Subárea dentro de cada área, como visto a seguir:'
 )
+
+# Configura o plot pelo plotly e o carrega usando a função correspondente do streamlit
 fig_subarea = px.sunburst(dados_df, path=["area", "subarea"])
 fig_subarea.update_traces(hovertemplate='Área/Subárea: %{id}' +
                           '<br>Contagem: %{value}',
@@ -113,14 +203,15 @@ st.plotly_chart(fig_subarea,
                 use_container_width=True,
                 **{'config': config_plotly})
 
-st.header('3. Requisitos de Semestralização/Previsão de Formatura')
+st.header('3. Requisitos de Semestralização')
 st.write(
     'Nesta seção, foram considerados equivalentes os requisitos de previsão de formatura e semestralização. Quando informado a previsão de formatura desejada pela empresa, este foi convertido em semestralização considerando o semestre em que o anuncio foi feito.'
 )
 st.write(
-    'Semestre 0 significa que não há requisito mínimo de semestre e semestre 10 significa que não há limite de semestralização máximo.'
+    'Semestre 0 significa que não há requisito mínimo de semestre e semestre 10 significa que não há limite de semestralização máximo. Lembrando que são considerados os dados informados pelas empresas, desconsiderando as limitações semestrais impostos pela UFBA'
 )
-# Manipulação de dados
+
+# Manipulação de dados contando o numero de anuncios por semestre e preenchendo os semestres sem anuncios com zeros
 index_semestres = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 requisito_semestre = dados_df['requisito_semestre'].value_counts(
     normalize=True).reindex(index_semestres, fill_value=0)
@@ -135,6 +226,7 @@ st.write(
     'Por outro lado, observou-se 1/4 das empresas requerem semestralização minima do quinto semestre e outros 1/4 no sexto semestre, excluindo dos processos seletivos os estudantes matriculados nos semestres iniciais'
 )
 
+# Configura o plot pelo plotly e o carrega usando a função correspondente do streamlit
 fig_requisito_semestre = go.Figure()
 fig_requisito_semestre.add_trace(
     go.Bar(
@@ -167,6 +259,7 @@ st.write(
     'Pelo lado da semestralização limite, quase 3/4 das ofertas de estágio não impôem limite de semestralização. O restante das ofertas de estágio possuem um limite máximo de semestralização do sexto ao nono semestre'
 )
 
+# Configura o plot pelo plotly e o carrega usando a função correspondente do streamlit
 fig_maximo_semestre = go.Figure()
 fig_maximo_semestre.add_trace(
     go.Bar(
@@ -197,10 +290,13 @@ st.subheader('4.1. Pré-requisitos mais comuns')
 st.write(
     'A seguir, observou-se os pré-requisitos mais comuns pedidos pelas empresas:'
 )
+
+# Pega os requisitos em cada anuncio, pegando cada subitem da lista de requisitos e os conta
 requisitos = pd.Series([
     item for sublist in dados_df.requisitos for item in sublist
 ]).value_counts()
 
+# Configura o plot pelo plotly e o carrega usando a função correspondente do streamlit
 fig_requisitos = go.Figure(
     data=[go.Bar(y=requisitos.index, x=requisitos, orientation='h')])
 fig_requisitos.update_traces(text=requisitos,
@@ -217,9 +313,11 @@ st.plotly_chart(fig_requisitos,
                 use_container_width=True,
                 **{'config': config_plotly})
 
+st.subheader('4.2. Existência de Pré-requisitos')
 st.write(
     'Verificou-se também a proporção entre empresas que pedem pré-requisitos nos anuncios de estágio e aquelas que não o fazem. Neste caso, mais da metade das empresas não demandam nenhum pré-requisito.'
 )
+
 # Contagem da quantidade de empresas que pedem pré-requisitos vs Empresas que não o fazem
 presenca_requisito_dict = {'Sem pré-requisitos': 0, 'Com pré-requisitos': 0}
 for requisito in dados_df['requisitos']:
@@ -230,7 +328,8 @@ for requisito in dados_df['requisitos']:
 df_presenca_requisito = pd.DataFrame.from_dict(presenca_requisito_dict,
                                                orient='index',
                                                columns=['contagem'])
-
+                                               
+# Configura o plot pelo plotly e o carrega usando a função correspondente do streamlit
 fig_presenca_requisito = go.Figure(data=[
     go.Pie(labels=df_presenca_requisito.index,
            values=df_presenca_requisito['contagem'],
@@ -254,16 +353,47 @@ st.plotly_chart(fig_presenca_requisito,
                 use_container_width=True,
                 **{'config': config_plotly})
 
-st.write('**@TODO: Tabela filtrando requisitos mais comuns por ÁREA**')
+st.subheader('4.3. Pré-requisitos filtrados por área de atuação')
+st.write('A seguir, podem ser filtrados os requisitos mais comuns por área:')
 
-st.subheader('4.2. Diferenciais mais comuns')
+# Pega os requisitos filtrados por área usando um selectbox para seleção
+filtro_area = st.selectbox('Escolha a área:', options=dados_df.area.unique())
+requisitos_filtrado = pd.Series([
+    item
+    for sublist in dados_df[dados_df['area'] == filtro_area.upper()].requisitos
+    for item in sublist
+]).value_counts()
+
+# Configura o plot pelo plotly e o carrega usando a função correspondente do streamlit
+fig_requisitos_filtrado = go.Figure(data=[
+    go.Bar(y=requisitos_filtrado.index, x=requisitos_filtrado, orientation='h')
+])
+fig_requisitos_filtrado.update_traces(
+    text=requisitos_filtrado,
+    textposition="outside",
+    hovertemplate='N° de anuncios: %{x}' +
+    '<br>Pré-requisito: %{y}<br><extra></extra>')
+fig_requisitos_filtrado.update_yaxes(autorange="reversed")
+fig_requisitos_filtrado.update_layout(margin=dict(l=0, r=0, b=20, t=20, pad=5),
+                                      height=500,
+                                      paper_bgcolor='rgba(0,0,0,0)',
+                                      plot_bgcolor='rgba(0,0,0,0)')
+
+st.plotly_chart(fig_requisitos_filtrado,
+                use_container_width=True,
+                **{'config': config_plotly})
+
+st.subheader('4.4. Diferenciais mais comuns')
 st.write(
     'Abaixo estão relacionados os diferenciais mais comuns, habilidades opcionais que concedem ao candidato uma vantagem em relação aos seus pares'
 )
+
+# Pega os diferenciais dos anuncios separadamente pegando cada subitem da lista de diferenciais e os conta
 diferenciais = pd.Series([
     item for sublist in dados_df.diferenciais for item in sublist
 ]).value_counts()
 
+# Configura o plot pelo plotly e o carrega usando a função correspondente do streamlit
 fig_diferenciais = go.Figure(
     data=[go.Bar(y=diferenciais.index, x=diferenciais, orientation='h')])
 fig_diferenciais.update_traces(text=diferenciais,
@@ -280,16 +410,20 @@ st.plotly_chart(fig_diferenciais,
                 use_container_width=True,
                 **{'config': config_plotly})
 
-st.header('Carga Horária Semanal')
+st.header('5. Carga Horária Semanal')
 st.write(
     'Em relação a carga horária dos estágios ofertados, a maioria dos anuncios de estágio não informam a carga horária requisitada ou possuem carga horária fléxivel.'
 )
 st.write(
     'Mais de 1/4 dos anuncios possuem a carga horária de 30 horas semanais, seguida das 20 horas semanais.'
 )
+
+# Pega as cargas horárias e os formata para melhor visualização
 dados_carga = dados_df['carga_horaria'].value_counts()
 dados_carga = dados_carga.add_suffix('h')
 dados_carga.rename(index={'0h': 'N/A'}, inplace=True)
+
+# Configura o plot pelo plotly e o carrega usando a função correspondente do streamlit
 fig_cargah = go.Figure(data=[
     go.Pie(labels=dados_carga.index,
            values=dados_carga,
@@ -311,15 +445,50 @@ st.plotly_chart(fig_cargah,
                 use_container_width=True,
                 **{'config': config_plotly})
 
-st.header('5. Ofertas por Empresa')
+st.header('6. Ofertas por Empresa')
 st.write(
-    '**@TODO: Barplot horizontal ou lista com empresas com mais ofertas de estágio**'
+    'Podemos verificar na nuvem de palavras a seguir o nome de todas as empresas que já realizaram anuncios através do Centro de Atendimento à Graduação - CEAG Politecnica e Colegiado de Engenharia Elétrica com vagas para Engenharia Elétrica/Eletrônica'
 )
+st.write(
+    'Observa-se que as empresas maiores na nuvem são as que possuem maior número de anuncios realizados no período estudado, demonstrando maior regularidade nas seleções de estágio'
+)
+
+# Conta a quantidade de anuncios por empresa e cria uma nuvem de palavras baseado na frequência destas
+dados_por_empresa = dados_df['empresa'].value_counts()
+wordcloud_empresa = WordCloud(
+    stopwords=STOPWORDS,
+    background_color='rgb(14, 17, 23)',
+    height=800,
+    width=800,
+    random_state=2,
+    colormap='rainbow',
+    collocations=False,
+).generate_from_frequencies(dados_por_empresa)
+st.image(wordcloud_empresa.to_array(), use_column_width='always')
+st.write('')
+
+st.header('7. Navegação pelos dados')
 st.write(
     'Pesquise abaixo o nome da empresa para ver todos os seus anuncios de estágio'
 )
 
-st.header(
-    '@TODO: Pegar se empresa fazia processo seletivo global ou anuncio apenas para elétrica'
-)
-st.write('**@TODO: Piechart mostrando a problematica acima**')
+# Realiza a filtragem dos dados baseado no nome da empresa digitado e dos dados a mostrar selecionados
+empresa_search = st.text_input(label="Digite o nome da empresa e dê enter:")
+columns = st.multiselect(label='Selecione os dados a mostrar:',
+                         options=dados_df.columns.to_list(),
+                         default=['empresa', 'periodo'])
+dados_df_filtrado = dados_df
+if empresa_search != '':
+    dados_df_filtrado = dados_df[dados_df['empresa'] == empresa_search.upper(
+    )]  
+    dados_df_filtrado = dados_df_filtrado[columns]
+else:
+    dados_df_filtrado = dados_df
+    dados_df_filtrado = dados_df_filtrado[columns]
+
+# Mostra o dataframe filtrado
+st.dataframe(dados_df_filtrado, height=400) 
+
+# Custom Footer
+st.markdown("""---""")
+st.markdown("""Feito com Streamlit, por [b-rbmp](https://github.com/b-rbmp/ "Perfil GitHub")""")
--- processamento.py
+++ processamento.py
@@ -3,7 +3,12 @@ import plotly.graph_objects as go
 import plotly.express as px
 from plotly.offline import download_plotlyjs, init_notebook_mode,  plot
 
-dados_df = pd.read_json('estagio_data.json', encoding='UTF-8')
+dados_df = pd.read_json('estagio_data.json', encoding='UTF-8', dtype='object')
 
-dados_por_empresa = dados_df['empresa'].value_counts()
+index_semestres = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+requisito_semestre = dados_df['requisito_semestre'].value_counts(
+    normalize=True).reindex(index_semestres, fill_value=0)
+maximo_semestre = dados_df['maximo_semestre'].value_counts(
+    normalize=True).reindex(index_semestres, fill_value=0)
+requisito_semestre.rename(index={0: 'N/A'}, inplace=True)
 #plot(fig)
